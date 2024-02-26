dc13ae2d4b728d47dd3f45e839251e4aeee9743d
SelvinVasquez <svasquezl9@miumg.edu.gt>
2024-02-26 16:35:24 -0600

    Prueba2

---
+++ main2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero, i=0;
+    cout<<"Ingrese un numero";
+    cin>>numero;
+    while (i <= numero){
+        if(i % 2 ==0){0
+            cout<<i<<endl;
+        }
+        i++;
+    }
+    return 0;
+}
